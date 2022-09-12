63b6f969ee6977bd013ebbdd32522759e6a607a8
GabrielBarbosaGomes <gabryel122crf@gmail.com>
2022-09-12 13:40:26 -0300

    ajusute de texte

--- src/components/SobreMim/index.tsx
+++ src/components/SobreMim/index.tsx
@@ -12,7 +12,7 @@ export function SobreMim(){
             <Content>
                 <img src={eu}/>
                 <p>
-                    Meu nome é Gabriel, tenho 24 anos, moro em Uberlândia-MG, sou desenvolvedor Front-end(ainda Jr) em busca de novos desafios, sempre tive interesse na programação, porém a vida é uma caixinha de surpresa, agora estou de volta e a todo vapor ingressando no mercado, estudando por conta e sendo mentorado por um dev sênior, Nas horas vagas eu pratico exercícios físicos como jogar bola, correr e ir à academia(princial kk), ou jogar online com meus amigos(o mais recorrente kk), gosto de ambientes descontraídos e que as pessoas não tenham medo de serem quem elas são.
+                    Meu nome é Gabriel, tenho 24 anos, moro em Uberlândia-MG, sou desenvolvedor Front-end em busca de novos desafios, sempre tive interesse na programação, porém a vida é uma caixinha de surpresa, agora estou de volta e a todo vapor ingressando no mercado, estudando por conta e sendo mentorado por um dev sênior, Nas horas vagas eu pratico exercícios físicos como jogar bola, correr e ir à academia, ou jogar online com meus amigos, gosto de ambientes descontraídos e que as pessoas não tenham medo de serem quem elas são.
                 </p>
             </Content>
         </Im>
